bf09b1311cdb27ae2496293616a953ca501e293e
defalt1337 <brandonprog@windowslive.com>
2021-02-18 01:23:23 +0100

    new method for button

--- 210215_regpage/script/snd.js
+++ 210215_regpage/script/snd.js
@@ -1,3 +1,8 @@
-document.getElementById("sendingbutton").onclick = function(){
-    window.location = "../wwlc.html";
-};
+var newtabwindows = "";
+var locationwindow = "../wwlc.html"
+function sendingbutton(){
+    newtabwindows = window.open(locationwindow);
+}
+// document.getElementById("sendingbutton").onclick = function(){
+//     window.location = "../wwlc.html";
+// };
