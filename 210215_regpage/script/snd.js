5066ad2f39d1aeb650417da612427c4591aba151
defalt1337 <brandonprog@windowslive.com>
2021-02-18 01:29:04 +0100

    fixed an issue

--- 210215_regpage/script/snd.js
+++ 210215_regpage/script/snd.js
@@ -1,6 +1,6 @@
 var newtabwindows = "";
 var locationwindow = "210215_regpage/wwlc.html"
-function sendingbutton(){
+function sendbutton(){
     newtabwindows = window.open(locationwindow);
 }
 // document.getElementById("sendingbutton").onclick = function(){
